52939c2d9c0b083d6fb5fdc513b7a420a8d15529
alkoral <35643632+alkoral@users.noreply.github.com>
2018-04-07 03:56:16 +0300

    Add files via upload

---
+++ templates/zz=etc/faqs_but.php
@@ -0,0 +1,220 @@
+<?php 
+include "header.php";
+
+    $db = Db::getConnection();
+    $faqsList = array();
+
+    $result = $db->query("SELECT * FROM topics ORDER BY category_id");
+
+    $i = 0;
+
+    while($row = $result->fetch()) {
+      $faqsList[$i]['id'] = $row['id'];
+      $faqsList[$i]['question'] = $row['question'];
+      $faqsList[$i]['answer'] = $row['answer'];
+      $faqsList[$i]['category_id'] = $row['category_id'];
+      $faqsList[$i]['author'] = $row['author'];
+      $faqsList[$i]['email'] = $row['email'];
+      $faqsList[$i]['date'] = $row['date'];
+      $faqsList[$i]['status'] = $row['status'];
+      $i++;
+      }
+    return $faqsList;
+
+echo $faqsList[$i]['question'];
+?>
+
+<div class="container"> <!-- Начало контейнера -->
+  <div class="row"> <!-- Начало ряда -->
+
+    <div class="col"> <!-- Начало блока вопросов -->
+      <h3>Вопросы и ответы</h3>
+
+      <div class="tabbable"> <!-- Начало таблицы  -->
+        <ul class="nav nav-pills nav-stacked col-md-3"> <!-- Начало левой колонки -->
+          <li class="active"><a href="#cat1" data-toggle="tab">Категория 1</a></li>
+          <li><a href="#cat2" data-toggle="tab">Категория 2</a></li>
+          <li><a href="#cat3" data-toggle="tab">Категория 3</a></li>
+        </ul> <!-- Конец левой колонки -->
+
+        <div class="tab-content col-md-9"> <!-- Начало правой колонки -->
+          
+          <div class="tab-pane active" id="cat1"> <!-- Начало блока вопросов и ответов для этой категории -->
+          	<h4>Категория 1</h4>
+            <div class="panel-group" id="collapse-group-1"> <!-- Начало группы вопросов и ответов -->
+              
+              <div class="panel panel-info"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el1" data-toggle="collapse" data-parent="#collapse-group-1">Первый вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el1" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+              </div> <!-- Конец вопроса и ответа -->
+              
+              <div class="panel panel-info"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el2" data-toggle="collapse" data-parent="#collapse-group-1">Второй вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el2" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+
+              <div class="panel panel-info"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el3" data-toggle="collapse" data-parent="#collapse-group-1">Третий вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el3" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+            </div> <!-- Конец группы вопросов и ответов -->
+          </div> <!-- Конец блока вопросов и ответов для этой категории -->
+
+          <div class="tab-pane" id="cat2"> <!-- Начало блока вопросов и ответов для этой категории -->
+          	<h4>Категория 2</h4>
+            <div class="panel-group" id="collapse-group-2"> <!-- Начало группы вопросов и ответов -->
+              
+              <div class="panel panel-warning"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el4" data-toggle="collapse" data-parent="#collapse-group-2">Первый вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el4" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+              
+              <div class="panel panel-warning"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el5" data-toggle="collapse" data-parent="#collapse-group-2">Второй вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el5" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+
+              <div class="panel panel-warning"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el6" data-toggle="collapse" data-parent="#collapse-group-2">Третий вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el6" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+            </div> <!-- Конец группы вопросов и ответов -->
+          </div> <!-- Конец блока вопросов и ответов для этой категории -->
+
+          <div class="tab-pane" id="cat3"> <!-- Начало блока вопросов и ответов для этой категории -->
+          	<h4>Категория 3</h4>
+            <div class="panel-group" id="collapse-group-3"> <!-- Начало группы вопросов и ответов -->
+              
+              <div class="panel panel-success"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el7" data-toggle="collapse" data-parent="#collapse-group-3">Первый вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el7" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+              
+              <div class="panel panel-success"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el8" data-toggle="collapse" data-parent="#collapse-group-3">Второй вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el8" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+
+              <div class="panel panel-success"> <!-- Вопрос и ответ -->
+
+                <div class="panel-heading"> <!-- Вопрос -->
+                  <a  href="#el9" data-toggle="collapse" data-parent="#collapse-group-3">Третий вопрос</a>
+                </div> <!-- Конец вопроса -->
+                
+                <div id="el9" class="collapse"> <!-- Ответ -->
+                  <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quidem delectus rerum eligendi mollitia, repudiandae quae beatae. Et repellat quam atque corrupti iusto architecto impedit explicabo repudiandae qui similique aut iure ipsum quis inventore nulla error aliquid alias quia dolorem dolore, odio excepturi veniam odit veritatis. Quo iure magnam, et cum. Laudantium, eaque non? Tempore nihil corporis cumque dolor ipsum accusamus sapiente aliquid quis ea assumenda deserunt praesentium voluptatibus, accusantium a mollitia necessitatibus nostrum voluptatem numquam modi ab, sint rem.
+                  </div>
+									<div class="panel-footer"> <!-- Начало футера к вопросу -->
+										Author | Email | Date
+									</div> <!-- Конец футера к вопросу -->
+                </div> <!-- Конец ответа -->
+                
+              </div> <!-- Конец вопроса и ответа -->
+            </div> <!-- Конец группы вопросов и ответов -->
+          </div> <!-- Конец блока вопросов и ответов для этой категории -->
+
+        </div> <!-- Конец правой колонки -->
+      </div> <!-- Конец таблицы  -->
+    </div> <!-- Конец блока вопросов -->
+  </div> <!-- Конец ряда -->
+</div> <!-- Конец контейнера -->
+
+<?php 
+include "footer.php";
+?>

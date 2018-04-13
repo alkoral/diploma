ea8c4d3d15e05d3dc0755ba4e87b1049b5a4b8d5
alkoral <35643632+alkoral@users.noreply.github.com>
2018-04-13 11:46:36 +0300

    Add files via upload

--- config/routes.php
+++ config/routes.php
@@ -2,22 +2,24 @@
 return array(
 	'index.php' => 'faqs/index', // вывод всего массива вопросов-ответов из базы / actionIndex in FaqsController
 
-	'faqs/([0-9]+)' => 'faqs/view/$1', // для просмотра вопроса-ответа по id, типа http://localhost/diploma/faqs/2
 	'faqs' => 'faqs/index', // вывод всего массива вопросов-ответов из базы / actionIndex in FaqsController
 	'add' => 'faqs/add', // вывод страницы для занесения в базу нового вопроса / actionAdd in FaqsController
-
-	'categories' => 'categories/index', // вывод всего массива категорий из базы / actionIndex in CategoriesController
-	'categories/([0-9]+)' => 'categories/view/$1', // для просмотра категории по id, типа http://localhost/diploma/categories/2
 
 	'login' => 'admins/login', // actionLogin in AdminsController
 	'logout' => 'admins/logout', // actionLogout in AdminsController
 
 	'admins' => 'admins/index', // actionIndex in AdminsController	
-	'admins/list' => 'admins/list', // actionList in AdminsController
 
+	'user/list' => 'user/list', // actionUserList in UserController
+	'user/delete/([0-9]+)' => 'user/delete/$1', // actionDelete in UserController
+	'user/create' => 'user/create', // actionCreate in UserController
+	'user/update/([0-9]+)' => 'user/update/$1', // actionUpdate in UserController
 
-	'topics' => 'topics/index', // админка: вывод вопросов-ответов 
-	'topics/edit/([0-9]+)' => 'topics/edit/$1', // админка: редактирование конкретного вопроса-ответов
+	'categories/list' => 'categories/list', // вывод всего массива категорий из базы / actionList in CategoriesController
+	'categories/create' => 'categories/create', // создание новой категории / actionCreate in CategoriesController
+	'categories/delete/([0-9]+)' => 'categories/delete/$1', // удаление категории вместе с вопросами в ней / actionDelete in CategoriesController
+
+	'topics/list' => 'topics/list', // вывод всех вопросов-ответов в админке
+	'topics/update/([0-9]+)' => 'topics/update/$1', // админка: редактирование конкретного вопроса-ответов
 	'topics/delete/([0-9]+)' => 'topics/delete/$1', // админка: удаление конкретного вопроса-ответа
-
 );

a177ca52190e234cc8bb1c2a1a00e90819bbd0a1
alkoral <35643632+alkoral@users.noreply.github.com>
2018-04-13 12:18:46 +0300

    Add files via upload

--- config/routes.php
+++ config/routes.php
@@ -5,21 +5,21 @@ return array(
 	'faqs' => 'faqs/index', // вывод всего массива вопросов-ответов из базы / actionIndex in FaqsController
 	'add' => 'faqs/add', // вывод страницы для занесения в базу нового вопроса / actionAdd in FaqsController
 
-	'login' => 'admins/login', // actionLogin in AdminsController
-	'logout' => 'admins/logout', // actionLogout in AdminsController
+	'login' => 'admins/login', // страница для входа в админку / actionLogin in AdminsController
+	'logout' => 'admins/logout', // страница для выхода из админки / actionLogout in AdminsController
 
-	'admins' => 'admins/index', // actionIndex in AdminsController	
+	'admins' => 'admins/index', // админка: вывод главной страницы / actionIndex in AdminsController	
 
-	'user/list' => 'user/list', // actionUserList in UserController
-	'user/delete/([0-9]+)' => 'user/delete/$1', // actionDelete in UserController
-	'user/create' => 'user/create', // actionCreate in UserController
-	'user/update/([0-9]+)' => 'user/update/$1', // actionUpdate in UserController
+	'user/list' => 'user/list', // админка: вывод всех админов / actionList in UserController
+	'user/delete/([0-9]+)' => 'user/delete/$1', // админка: удаление админа / actionDelete in UserController
+	'user/create' => 'user/create', // админка: создание админа / actionCreate in UserController
+	'user/update/([0-9]+)' => 'user/update/$1', // админка: изменение пароля админа / actionUpdate in UserController
 
 	'categories/list' => 'categories/list', // вывод всего массива категорий из базы / actionList in CategoriesController
-	'categories/create' => 'categories/create', // создание новой категории / actionCreate in CategoriesController
-	'categories/delete/([0-9]+)' => 'categories/delete/$1', // удаление категории вместе с вопросами в ней / actionDelete in CategoriesController
+	'categories/create' => 'categories/create', // админка: создание новой категории / actionCreate in CategoriesController
+	'categories/delete/([0-9]+)' => 'categories/delete/$1', // админка: удаление категории вместе с вопросами в ней / actionDelete in CategoriesController
 
-	'topics/list' => 'topics/list', // вывод всех вопросов-ответов в админке
-	'topics/update/([0-9]+)' => 'topics/update/$1', // админка: редактирование конкретного вопроса-ответов
-	'topics/delete/([0-9]+)' => 'topics/delete/$1', // админка: удаление конкретного вопроса-ответа
+	'topics/list' => 'topics/list', // вывод всех вопросов-ответов в админке / actionList in TopicsController
+	'topics/update/([0-9]+)' => 'topics/update/$1', // админка: редактирование конкретного вопроса-ответов / actionUpdate in TopicsController
+	'topics/delete/([0-9]+)' => 'topics/delete/$1', // админка: удаление конкретного вопроса-ответа / actionDelete in TopicsController
 );

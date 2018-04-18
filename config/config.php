4114ea18e819df8152862fcb483e42bf9287dc1a
alkoral <35643632+alkoral@users.noreply.github.com>
2018-04-18 15:50:27 +0300

    Delete config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once 'vendor/autoload.php';
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader, array (
-	'cache'=>'tmp/cache',
-	'auto_reload'=>true
-));
-$twig->addGlobal('session', $_SESSION);
-

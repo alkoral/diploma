6ee16d59fdd9d082657a0246343c84fea90673ef
alkoral <35643632+alkoral@users.noreply.github.com>
2018-04-18 15:49:37 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -1,11 +1,6 @@
 <?php
 
 return array(
-//	'host' => '127.0.0.1',
-//	'dbname' => 'korzun',
-//	'user' => 'korzun',
-//	'password' => 'neto1653',
-
 	'host' => 'localhost',
 	'dbname' => 'diploma',
 	'user' => 'root',
